84ff97125ccb42b93de8bc0c9f2030757bd30e0c
axelrammon <axelrammon48@gmail.com>
2022-06-24 11:20:32 -0300

    add curso sobre useState and useEffect

---
+++ reactJS/stateAndEffect/aula-hooks/src/Components/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+// import Counter from '../Counter/Counter';
+// import SmartCounter from '../SmartCounter/SmartCounter';
+import Ifoodcounter from '../Ifoodcounter/Ifoodcounter';
+
+function App() {
+  return (
+    <div>
+      <Ifoodcounter />
+
+    </div>
+  )
+}
+
+export default App;
